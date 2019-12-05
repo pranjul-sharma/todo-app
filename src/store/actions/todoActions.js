5cb1c8cc71547609ab5e548ca1ff87728735963e
Pranjul Sharma <pranjuls@geekyants.com>
2019-12-05 23:43:06 +0530

    (feat/thunk)-modified actions to use thunk for async actions

--- src/store/actions/todoActions.js
+++ src/store/actions/todoActions.js
@@ -1,20 +1,36 @@
 import * as aTypes from '../../shared/actionTypes';
+import Api from '../../api/todoApi';
 let currId = 0;
 
-export const addTodo = task => ({
-  type: aTypes.ADD_TODO,
-  payload: {
-    id: ++currId,
-    task
-  }
-});
+export const addTodo = task => dispatch => {
+  dispatch({ type: aTypes.ADD_TODO_LOADING });
+  Api.addTodo()
+    .then(res =>
+      dispatch({
+        type: aTypes.ADD_TODO,
+        payload: {
+          id: ++currId,
+          task
+        }
+      })
+    )
+    .catch(e => dispatch({ type: aTypes.ADD_TODO_FAILED, message: e.message }));
+};
 
-export const toggleTodo = id => ({
-  type: aTypes.TOGGLE_TODO,
-  payload: { id }
-});
+export const toggleTodo = id => dispatch => {
+  dispatch({ type: aTypes.TOGGLE_TODO_LOADING });
+  Api.toggleTodo()
+    .then(res => dispatch({ type: aTypes.TOGGLE_TODO, payload: { id } }))
+    .catch(e =>
+      dispatch({ type: aTypes.TOGGLE_TODO_FAILED, message: e.message })
+    );
+};
 
-export const deleteTodo = id => ({
-  type: aTypes.DELETE_TODO,
-  payload: { id }
-});
+export const deleteTodo = id => dispatch => {
+  dispatch({ type: aTypes.DELETE_TODO_LOADING });
+  Api.deleteTodo()
+    .then(res => dispatch({ type: aTypes.DELETE_TODO, payload: { id } }))
+    .catch(e =>
+      dispatch({ type: aTypes.DELETE_TODO_FAILED, message: e.message })
+    );
+};
