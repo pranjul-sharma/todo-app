e3f26a48e5d9f28ba81f2e05468d4a201473a75e
Pranjul Sharma <pranjuls@geekyants.com>
2019-12-05 22:09:10 +0530

    (feat/todo)-added todo to store data in app's state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import TodoTask from './components/todo-task/TodoTask';
+import AddTodoInput from './components/add-todo-input/AddTodoInput';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      inputVal: '',
+      currId: 0
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('handle submit');
+    let task = {
+      id: this.state.currId,
+      task: this.state.inputVal,
+      isCompleted: false
+    };
+    let tasks = this.state.tasks;
+    tasks.push(task);
+    this.setState({
+      tasks: tasks,
+      inputVal: '',
+      currId: this.state.currId + 1
+    });
+  };
+
+  handleInputChange = e => {
+    const val = e.target.value;
+    console.log('Handle Input Change: ', val, e);
+    this.setState({
+      inputVal: val
+    });
+  };
+
+  handleCheckChange = idx => {
+    console.log(idx);
+    let tasks = this.state.tasks;
+    tasks[idx] = Object.assign({}, tasks[idx], {
+      isCompleted: !tasks[idx].isCompleted
+    });
+    this.setState({
+      tasks: tasks
+    });
+  };
+
+  handleDeleteClick = idx => {
+    let tasks = this.state.tasks;
+    tasks.splice(idx, 1);
+    this.setState({
+      tasks: tasks
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Header bgColor="#333" />
+        <div className="content">
+          <AddTodoInput
+            handleInputChange={this.handleInputChange}
+            value={this.state.inputVal}
+            handleSubmit={this.handleSubmit}
+          />
+          {JSON.stringify(this.props.tasks)}
+          {this.state.tasks.map(task => (
+            <TodoTask
+              key={task.id}
+              task={task}
+              handleCheckChange={this.handleCheckChange}
+              handleDeleteClick={this.handleDeleteClick}
+            />
+          ))}
+          {!this.state.tasks.length && (
+            <div className="no-data">Start adding todo now.</div>
+          )}
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
