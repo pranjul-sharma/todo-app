97adcf9c791acd31cbd31323eef43e6d0b1c9835
Pranjul Sharma <pranjuls@geekyants.com>
2019-12-05 22:47:03 +0530

    (feat/redux)-added actiontypes and functions to store data in redux

---
+++ src/store/actions/todoActions.js
@@ -0,0 +1,20 @@
+import * as aTypes from '../../shared/actionTypes';
+let currId = 0;
+
+export const addTodo = task => ({
+  type: aTypes.ADD_TODO,
+  payload: {
+    id: ++currId,
+    task
+  }
+});
+
+export const toggleTodo = id => ({
+  type: aTypes.TOGGLE_TODO,
+  payload: { id }
+});
+
+export const deleteTodo = id => ({
+  type: aTypes.DELETE_TODO,
+  payload: { id }
+});
