f91f2cfec393cf37dbdd7f46df3279cb85ee4b70
Pranjul Sharma <pranjuls@geekyants.com>
2019-12-05 22:48:43 +0530

    (feat/redux)-added redux store and removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,61 +1,62 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 import Header from './components/header/Header';
 import TodoTask from './components/todo-task/TodoTask';
 import AddTodoInput from './components/add-todo-input/AddTodoInput';
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tasks: [],
-      inputVal: '',
-      currId: 0
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     tasks: [],
+  //     inputVal: '',
+  //     currId: 0
+  //   };
+  // }
 
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log('handle submit');
-    let task = {
-      id: this.state.currId,
-      task: this.state.inputVal,
-      isCompleted: false
-    };
-    let tasks = this.state.tasks;
-    tasks.push(task);
-    this.setState({
-      tasks: tasks,
-      inputVal: '',
-      currId: this.state.currId + 1
-    });
-  };
+  // handleSubmit = e => {
+  //   e.preventDefault();
+  //   console.log('handle submit');
+  //   let task = {
+  //     id: this.state.currId,
+  //     task: this.state.inputVal,
+  //     isCompleted: false
+  //   };
+  //   let tasks = this.state.tasks;
+  //   tasks.push(task);
+  //   this.setState({
+  //     tasks: tasks,
+  //     inputVal: '',
+  //     currId: this.state.currId + 1
+  //   });
+  // };
 
-  handleInputChange = e => {
-    const val = e.target.value;
-    console.log('Handle Input Change: ', val, e);
-    this.setState({
-      inputVal: val
-    });
-  };
+  // handleInputChange = e => {
+  //   const val = e.target.value;
+  //   console.log('Handle Input Change: ', val, e);
+  //   this.setState({
+  //     inputVal: val
+  //   });
+  // };
 
-  handleCheckChange = idx => {
-    console.log(idx);
-    let tasks = this.state.tasks;
-    tasks[idx] = Object.assign({}, tasks[idx], {
-      isCompleted: !tasks[idx].isCompleted
-    });
-    this.setState({
-      tasks: tasks
-    });
-  };
+  // handleCheckChange = idx => {
+  //   console.log(idx);
+  //   let tasks = this.state.tasks;
+  //   tasks[idx] = Object.assign({}, tasks[idx], {
+  //     isCompleted: !tasks[idx].isCompleted
+  //   });
+  //   this.setState({
+  //     tasks: tasks
+  //   });
+  // };
 
-  handleDeleteClick = idx => {
-    let tasks = this.state.tasks;
-    tasks.splice(idx, 1);
-    this.setState({
-      tasks: tasks
-    });
-  };
+  // handleDeleteClick = idx => {
+  //   let tasks = this.state.tasks;
+  //   tasks.splice(idx, 1);
+  //   this.setState({
+  //     tasks: tasks
+  //   });
+  // };
 
   render() {
     return (
@@ -63,20 +64,19 @@ class App extends React.Component {
         <Header bgColor="#333" />
         <div className="content">
           <AddTodoInput
-            handleInputChange={this.handleInputChange}
-            value={this.state.inputVal}
-            handleSubmit={this.handleSubmit}
+          /* handleInputChange={this.handleInputChange} */
+          /* value={this.state.inputVal} */
+          /* handleSubmit={this.handleSubmit} */
           />
-          {JSON.stringify(this.props.tasks)}
-          {this.state.tasks.map(task => (
+          {this.props.tasks.map(task => (
             <TodoTask
               key={task.id}
               task={task}
-              handleCheckChange={this.handleCheckChange}
-              handleDeleteClick={this.handleDeleteClick}
+              /* handleCheckChange={this.handleCheckChange}
+              handleDeleteClick={this.handleDeleteClick} */
             />
           ))}
-          {!this.state.tasks.length && (
+          {!this.props.tasks.length && (
             <div className="no-data">Start adding todo now.</div>
           )}
         </div>
@@ -85,4 +85,14 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log('state: ', state);
+  return {
+    tasks: state.todos.tasks
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(App);
