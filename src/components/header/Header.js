237bdf1a782c89ac0e7815d8d13ba66260db44a5
Pranjul Sharma <pranjuls@geekyants.com>
2019-12-05 22:07:49 +0530

    (feat/todo)-added Header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+function Header(props) {
+  return (
+    <div className="header">
+      <h1>Hello TodoApp</h1>
+    </div>
+  );
+}
+
+export default Header;
