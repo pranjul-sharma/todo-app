c3b1eab175820b041787e21e6366b7456e036656
Pranjul Sharma <pranjuls@geekyants.com>
2019-12-06 00:01:07 +0530

    (feat/saga)-modified store to apply saga as middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../saga';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const sagaMiddleware = createSagaMiddleware();
 export default function configureStore() {
   const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(applyMiddleware(sagaMiddleware))
   );
+  sagaMiddleware.run(rootSaga);
   return store;
 }
